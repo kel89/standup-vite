7305a45cfa3247216989f7caeb71b3cff34e27ce
Ken <kel89@cornell.edu>
2025-09-21 21:55:03 -0600

    fix build

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// @ts-ignore
 import App from "./App.jsx";
 import "./index.css";
-import { Amplify } from "aws-amplify";
+// import { Amplify } from "aws-amplify";
 // import outputs from "../amplify_outputs.json";
 
 // Amplify.configure(outputs);
